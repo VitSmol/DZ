5625d7e52c18502b2a9a0f4a509aa47940fd1a70
VitSmol <smolitskiy@mail.ru>
2022-03-23 09:19:02 +0300

    fix: add some fix

--- dz1/js/app.js
+++ dz1/js/app.js
@@ -1,27 +1,32 @@
 const contentBlock = document.querySelectorAll('.content')
-const infoContainer = document.querySelector(".info").clientWidth
 const links = document.querySelectorAll('.link')
 const contentContainer = document.querySelector(`.content__container`)
+const infoContainer = document.querySelector(".info").clientWidth
 
 const inputElement = document.querySelector('#checkbox')
 const flexStyle = "css/style1.css"
 const floatStyle = "css/style2.css"
 const style = document.querySelector(`#style`)
-
+let docBody = document.body
 
 let currentWidth = infoContainer;
 let tempInd;
-if (window.outerWidth <= 820) {
-    contentBlock.forEach(el => {
-        console.log(el);
-        el.style.width = infoContainer + 'px'
-    })
-}
 
-window.onresize = () => {
+const changeWidthByLoad = () => {
+    console.log(`change width`);
+    if (docBody.clientWidth <= 820) {
+        contentBlock.forEach(el => {
+            el.style.width = infoContainer + 'px'
+        })
+    }
+}
+changeWidthByLoad()
+
+const changeWidth = () => {
     const infoContainer = document.querySelector(".info").clientWidth
+    const contentBlock = document.querySelectorAll('.content')
     contentBlock.forEach(el => {
-        if (window.outerWidth <= 820) {
+        if (docBody.clientWidth <= 820) {
                 el.style.width = infoContainer + 'px'
                 contentContainer.style.transform = `translate(-${tempInd * parseInt(infoContainer)}px)`;
         } else {
@@ -30,6 +35,10 @@ window.onresize = () => {
         }
         currentWidth = el.style.width
     })
+}
+
+docBody.onresize = () => {
+    changeWidth()
 }
 
 links.forEach((el, ind) => {
@@ -47,5 +56,11 @@ links.forEach((el, ind) => {
 })
 
 inputElement.addEventListener(`click`, function() {
-    this.checked == true ? style.setAttribute(`href`, flexStyle) : style.setAttribute(`href`, floatStyle)
+    // this.checked == true ? (style.setAttribute(`href`, flexStyle), changeWidth()) : (style.setAttribute(`href`, floatStyle), changeWidth())
+    changeWidth()
+    if (this.checked == true) {
+        style.setAttribute(`href`, flexStyle);
+    } else {
+        style.setAttribute(`href`, floatStyle);
+    }
 })
