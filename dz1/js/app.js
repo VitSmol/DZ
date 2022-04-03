dc6bb6bc4879b78806714637a6898e9b4c0be557
VitSmol <87019119+VitSmol@users.noreply.github.com>
2022-04-03 16:39:04 +0300

    Delete dz1 directory

--- dz1/js/app.js
+++
@@ -1,7 +0,0 @@
-const style = document.querySelector(`#style`)
-const inputElement = document.getElementById(`checkbox`)
-
-inputElement.addEventListener(`click`, function() {
-    style.hasAttribute(`disabled`) ? style.removeAttribute(`disabled`) : style.setAttribute(`disabled`, `true`);
-    // this.checked == true ? (style.setAttribute(`href`, flexStyle), changeWidth()) : (style.setAttribute(`href`, floatStyle), changeWidth())
-})
