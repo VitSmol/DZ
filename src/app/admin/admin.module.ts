c36c14148cee157a2386f9a5f0a157ba8d5af431
VitSmol <smolitskiy@mail.ru>
2022-05-31 12:04:39 +0300

    feat: add routing

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { AddPageComponent } from "./add-page/add-page.component";
+import { DashboardPageComponent } from "./dashboard-page/dashboard-page.component";
+import { EditPageComponent } from "./edit-page/edit-page.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { AdminLayoutComponent } from "./shared/admin-layout/admin-layout.component";
+
+@NgModule({
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    EditPageComponent,
+    AddPageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', component: AdminLayoutComponent, children: [
+        {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
+        {path: 'login', component: LoginPageComponent},
+        {path: 'dashboard', component: DashboardPageComponent},
+        {path: 'doctor/:id/edit', component: EditPageComponent},
+        {path: 'add', component: AddPageComponent},
+      ]}
+    ])
+  ],
+  exports: [RouterModule]
+})
+
+export class AdminModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,28 @@ import { DoctorsComponent } from './doctors/doctors.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DoctorsComponent
+    DoctorsComponent,
+    MainLayoutComponent,
+    DoctorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
-    MatSortModule
+    MatSortModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
