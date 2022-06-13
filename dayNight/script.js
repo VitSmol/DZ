10b56c28f82ef89967bba7c4ba46f4bf113a9b89
VitSmol <smolitskiy@mail.ru>
2022-06-13 05:31:01 +0300

    feat: add solution

--- dayNight/script.js
+++ dayNight/script.js
@@ -1,8 +1,3 @@
-// Develop page with two dropdowns “Title” and “Category”
-// Values of selections should invoke render of data from https://api.publicapis.org/
-// Handle possible error cases
-// Push code to repo “Asia chronicle”
-
 let categorySelect = document.getElementById('category');
 
 async function getArticles() {
