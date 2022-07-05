3f6a6460ee9a7f58d92b1f30d873774a0274d749
VitSmol <smolitskiy@mail.ru>
2022-07-05 12:45:26 +0300

    feat: fix

--- sweet/app.js
+++ sweet/app.js
@@ -12,35 +12,71 @@ class Entity {
 class Box extends Entity {
     constructor(name) {
         super(name)
+        this.stuffs = [];
+        this.usedBox = false
     }
-    stuff = [];
+    addStufs(...newSuffs) {
+        this.stuffs = [...this.stuffs, ...newSuffs]
+    }
+    showStuffs() {
+        return `${this.name} box contains ${this.stuffs.join(`,`)}`
+    }
 }
 
 class Stuff extends Entity {
     constructor(name) {
         super(name);
-        this.name = name;
+    }
+    showName() {
+        return `${this.name}`
     }
 }
 
 class User extends Entity {
     constructor(name) {
         super(name);
-        this.name = name;
     }
-    property = [];
+    setBox(box) {
+        if (!box.usedBox) {
+            this.box = box;
+            this.box.usedBox = true
+        } else {
+            console.log(`This box cannot be owned by the user ${this.name}`);
+        }
+    }
 }
+// set Stuffs
+let stuff1 = new Stuff(`pen`);
+let stuff2 = new Stuff(`pencil`);
+//set boxes
+const box1 = new Box(`Pencil Box`)
+const box2 = new Box(`Pencil Box`)
+const box3 = new Box(`Other Pencil Box`)
 
-let pencilBox = new Box('pencilBox'),
-    pen = new Stuff('pen'),
-    pencil = new Stuff('pencil'),
-    user = new User('Vitaliy');
+//add stuffs to boxes
+box1.addStufs(stuff1, stuff2);
+box2.addStufs(stuff1, stuff2);
+box3.addStufs(stuff1, stuff2);
 
-pencilBox.stuff.push(pen.name);
-pencilBox.stuff.push(pencil.name);
+//create first user and give him the box1
+const user1 = new User(`Vit`);
+user1.setBox(box1)
+//create second user and give him the box2
+const user2 = new User(`Ned`); 
+user2.setBox(box2)
+//create third user and try to give him box1 and box2
+const user3 = new User(`Tyrion`); 
+user3.setBox(box1)
+user3.setBox(box2)
+//Now try to give User3 another box
+user3.setBox(box3)
 
-if (user.name === 'Vitaliy') {
-    user.property.push(pencilBox);
-} else console.log('Not the property of the user')
-
-console.log(user.property);
+console.log(
+    JSON.stringify(user1)
+);
+console.log(
+    JSON.stringify(user2)
+);
+console.log(
+    JSON.stringify(user3)
+);
